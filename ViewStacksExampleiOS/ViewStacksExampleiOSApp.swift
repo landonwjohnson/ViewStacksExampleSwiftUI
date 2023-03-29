66812ed31e11aab9a4bd9070550018ceeec42338
“Landon <landonwjohnson@gmail.com>
2023-03-29 10:48:34 -0600

    Initial Commit

---
+++ ViewStacksExampleiOS/ViewStacksExampleiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewStacksExampleiOSApp.swift
+//  ViewStacksExampleiOS
+//
+//  Created by Landon Johnson on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewStacksExampleiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
