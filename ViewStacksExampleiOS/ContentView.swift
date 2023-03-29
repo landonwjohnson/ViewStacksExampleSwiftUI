5398902ad789d14dd3a06e0ae225a2120d6acba1
“Landon <landonwjohnson@gmail.com>
2023-03-29 12:10:50 -0600

    Example project setup

--- ViewStacksExampleiOS/ContentView.swift
+++ ViewStacksExampleiOS/ContentView.swift
@@ -1,21 +1,137 @@
+
+import SwiftUI
+
+
 //
 //  ContentView.swift
-//  ViewStacksExampleiOS
+//  StackViewsExample
 //
 //  Created by Landon Johnson on 3/29/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct ImageInfo:  View {
+    var title: String;
+    var color: Color;
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack() {
+            Color("TextDrop").edgesIgnoringSafeArea(.all)
+                .frame(height: 80)
+                .cornerRadius(7)
+            
+            ZStack{
+                Text(title)
+                    .foregroundColor(color)
+                    .font(.system(size: 28))
+                    .shadow(color: Color("TextDrop"), radius: 8)
+                    .padding(10)
+            }
         }
-        .padding()
+    }
+}
+
+struct ExampleOne: View {
+    var body: some View {
+        VStack() {
+            Image("LightPaintingOne")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(7)
+            
+            ImageInfo(title: "Light Painting", color: Color.white)
+        }
+    }
+}
+
+struct ExampleTwo: View {
+    var body: some View {
+        HStack() {
+            // By default the views inside the HStack would
+            // render starting from the left
+            // Image would render first
+            Image("LightPaintingOne")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(7)
+            
+            ImageInfo(title: "Light Painting", color: Color.white)
+        }
+    }
+}
+
+struct ExampleThree: View {
+    var body: some View {
+        ZStack() {
+            // Image would render first (Underneath)
+            Image("LightPaintingTwo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(7)
+            
+            // ImageInfo would render on top
+            ImageInfo(title: "Light Painting", color: Color.white)
+        }
+    }
+}
+
+
+enum SelectableViews: String, CaseIterable   {
+    case VStack
+    case HStack
+    case ZStack
+}
+
+func getViewForPicker (viewName:String) -> some View {
+    switch(viewName){
+    case "VStack":
+        return ExampleOne()
+    case "HStack":
+        return ExampleTwo()
+    case "ZStack":
+        return ExampleThree()
+    default:
+        return ExampleOne()
+    }
+    
+}
+
+struct ContentView: View {
+    @State public var selectedView = "VStack"
+    
+    let viewList: [String] = SelectableViews.allCases.map { $0.rawValue }
+    var viewToRender: some View  {
+        return getViewForPicker(viewName: selectedView)
+    }
+    
+    
+    var body: some View {
+        ZStack{
+            Color("Backdrop").edgesIgnoringSafeArea(.all)
+            ZStack{
+                VStack{
+                    Spacer()
+                    VStack(alignment: .center){
+                        viewToRender
+                    }.padding(20)
+                    Spacer()
+                    Spacer()
+                    ZStack{
+                        Color.white
+                        VStack{
+                            Picker("StackView", selection: $selectedView) {
+                                ForEach(viewList, id: \.self) {
+                                    Text($0)
+                                }
+                            }.pickerStyle(.segmented)
+                        }.frame(height: 80)
+                        
+                        
+                    }.frame(height: 50)
+                }
+            }
+        }
+        
     }
 }
 
